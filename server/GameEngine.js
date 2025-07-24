c85f95dac05c24dbda9f5d78572213a823cc4ac9
Gato <gatocorreo27@gmail.com>
2025-07-24 01:11:42 +0000

    Added foundations for Game Engine

---
+++ server/GameEngine.js
@@ -0,0 +1,17 @@
+class Piece {
+    constructor (controllerId) {
+        this.controllerId = controllerId;
+    }
+}
+
+class GameEngine {
+    constructor () {
+        this.pieces = Map();
+        this.world = null;
+    }
+
+    loadMap(path) {
+        
+    }
+
+}
